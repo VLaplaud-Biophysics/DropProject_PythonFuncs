38069cc43b61379274aec30617e817e1ed5595cf
ValentinLAPLAUD <vallap.biophysics@protonmail.com>
2023-11-02 13:21:47 +0100

    Added FixedDrop diagrams

--- ImpactSimulationFuncs.py
+++ ImpactSimulationFuncs.py
@@ -68,9 +68,9 @@ def plotFracs(Angle,npts,DropDiam,ConeDiams):
         ax2.legend()
         
 ###
-# 2. Phase diagrams
+# 2. Fixed angle diagrams
 
-def PhaseDiagrams(Angle,ConeDiam,RelDropDiams,RelOffCents):
+def FixedAngleDiagrams(Angle,ConeDiam,RelDropDiams,RelOffCents,oriType):
     
     npts = len(RelDropDiams)
     
@@ -107,7 +107,7 @@ def PhaseDiagrams(Angle,ConeDiam,RelDropDiams,RelOffCents):
         if oc < OffCmax:
 
             Drop = dgc.Drop(dd/2,oc,50,DropSpeed)
-            Impact = Cone.impact(Drop,'Hmax')
+            Impact = Cone.impact(Drop,oriType)
             JetFracs[i] = Impact.compute_JetFrac()
             JetNRJ[i] = Impact.VolFrac*Impact.JetFrac/100*4/3*np.pi*(dd/2)**3*rho*DropSpeed**2 # [J]
             # JetFrac[%] * VolFrac[U] * VolDrop[mm3] * WaterDensity[kg/mm3] * DropSpeed²[mm²/ms²] = JetKineticNRJ[kg.mm²/ms² = J]
@@ -123,7 +123,7 @@ def PhaseDiagrams(Angle,ConeDiam,RelDropDiams,RelOffCents):
     
     # Impact fraction in jet
     fig0,ax0 = plt.subplots(dpi=150,figsize = (7,6)) 
-    ax0.set_title('Cone angle = ' + str(Angle/(2*np.pi)*360) + '°')
+    ax0.set_title('Cone angle = ' + str(Angle/(2*np.pi)*360))
     ax0.set_xlabel('Offcent/ConeRadius')
     ax0.set_ylabel('DropSize/ConeSize')
     
@@ -136,7 +136,7 @@ def PhaseDiagrams(Angle,ConeDiam,RelDropDiams,RelOffCents):
 
     # Sheet/Jet volume ratio
     fig1,ax1 = plt.subplots(dpi=150,figsize = (7,6)) 
-    ax1.set_title('Cone angle = ' + str(Angle/(2*np.pi)*360) + '°')
+    ax1.set_title('Cone angle = ' + str(Angle/(2*np.pi)*360))
     ax1.set_xlabel('Offcent/ConeRadius')
     ax1.set_ylabel('DropSize/ConeSize')
     
@@ -150,7 +150,7 @@ def PhaseDiagrams(Angle,ConeDiam,RelDropDiams,RelOffCents):
     
     # Sheet/Jet volume ratio
     fig2,ax2 = plt.subplots(dpi=150,figsize = (7,6)) 
-    ax2.set_title('Cone angle = ' + str(Angle/(2*np.pi)*360) + '°')
+    ax2.set_title('Cone angle = ' + str(Angle/(2*np.pi)*360))
     ax2.set_xlabel('Offcent/ConeRadius')
     ax2.set_ylabel('DropSize/ConeSize')
     
@@ -162,11 +162,124 @@ def PhaseDiagrams(Angle,ConeDiam,RelDropDiams,RelOffCents):
     
     # kinetic energy in the jet
     fig3,ax3 = plt.subplots(dpi=150,figsize = (7,6)) 
-    ax3.set_title('Cone angle = ' + str(Angle/(2*np.pi)*360) + '°')
+    ax3.set_title('Cone angle = ' + str(Angle/(2*np.pi)*360))
     ax3.set_xlabel('Offcent/ConeRadius')
     ax3.set_ylabel('DropSize/ConeSize')
     
-    sc3 = ax3.scatter(meshOC,meshDD,c=JetNRJ*1000,cmap='jet',s=100)
+    sc3 = ax3.scatter(meshOC,meshDD,c=JetNRJ*1000,cmap='jet',s=pointSize)
+    
+    cbar3 = plt.colorbar(sc3)
+    cbar3.set_label('Maximum kinetic energy in jet [mJ]')
+    fig3.tight_layout()
+
+    return(fig0,fig1,fig2,fig3)
+
+###
+# 2. Energy diagrams
+
+def FixedDropDiagrams(RelDropDiam,ConeDiam,Angles,RelOffCents,oriType):
+    
+    npts = len(Angles)
+    
+    meshA,meshOC = np.meshgrid(Angles,RelOffCents)
+    
+    meshA = meshA.flatten()
+    meshOC = meshOC.flatten()
+    
+    DropSpeed = 5 # [mm/ms]
+    rho = 997e-9 # [kg/mm3]
+    
+    JetFracs = np.empty(np.shape(meshA))
+    SheetWide = np.empty(np.shape(meshA))
+    JetNRJ = np.empty(np.shape(meshA))
+    
+
+    dd = RelDropDiam*ConeDiam
+    
+    nsim = len(meshOC)
+    
+    cpt = 0
+    
+    for ra,roc,i in zip(meshA,meshOC,range(nsim)):
+        
+        cpt += 1
+
+        print(f'Computing impacts n°{cpt:1d} of {nsim:1d}.', end='\r')
+            
+        
+        OffCmax = 0.495*(ConeDiam+dd)
+        
+        oc = roc*ConeDiam/2
+            
+        if oc < OffCmax:
+            
+            Cone = dgc.Cone(ConeDiam/2,ra)
+
+            Drop = dgc.Drop(dd/2,oc,50,DropSpeed)
+            Impact = Cone.impact(Drop,oriType)
+            JetFracs[i] = Impact.compute_JetFrac()
+            JetNRJ[i] = Impact.VolFrac*Impact.JetFrac/100*4/3*np.pi*(dd/2)**3*rho*DropSpeed**2 # [J]
+            # JetFrac[%] * VolFrac[U] * VolDrop[mm3] * WaterDensity[kg/mm3] * DropSpeed²[mm²/ms²] = JetKineticNRJ[kg.mm²/ms² = J]
+            SheetWide[i] = Impact.SheetOpening()[0]
+            
+        else:
+            
+            JetFracs[i] = np.nan
+            SheetWide[i] = np.nan
+            JetNRJ[i] = np.nan
+       
+    pointSize = 250000/npts**2
+    
+    # Impact fraction in jet
+    fig0,ax0 = plt.subplots(dpi=150,figsize = (7,6)) 
+    ax0.set_title('DropSize/ConeSize : ' + str(RelDropDiam) + '°')
+    ax0.set_xlabel('Offcent/ConeRadius')
+    ax0.set_ylabel('Cone angle [°]')
+    ax0.set_xlim([0,np.max(RelOffCents)])
+    
+    
+    sc0 = ax0.scatter(meshOC,meshA/(2*np.pi)*360,c=JetFracs,cmap='PuOr',s=pointSize)
+
+    cbar0 = plt.colorbar(sc0)
+    cbar0.set_label('Impact fraction in the jet (%)')
+    fig0.tight_layout()
+
+    # Sheet/Jet volume ratio
+    fig1,ax1 = plt.subplots(dpi=150,figsize = (7,6)) 
+    ax1.set_title('DropSize/ConeSize : ' + str(RelDropDiam) + '°')
+    ax1.set_xlabel('Offcent/ConeRadius')
+    ax1.set_ylabel('Cone angle [°]')
+    ax1.set_xlim([0,np.max(RelOffCents)])
+    
+    
+    sc1 = ax1.scatter(meshOC,meshA/(2*np.pi)*360,c=np.divide(np.subtract(100,JetFracs),JetFracs),cmap='plasma',s=pointSize)
+    
+    cbar1 = plt.colorbar(sc1)
+    cbar1.set_label('Sheet/Jet volume ratio')
+    fig1.tight_layout()
+    
+    
+    # Sheet/Jet volume ratio
+    fig2,ax2 = plt.subplots(dpi=150,figsize = (7,6)) 
+    ax2.set_title('DropSize/ConeSize : ' + str(RelDropDiam) + '°')
+    ax2.set_xlabel('Offcent/ConeRadius')
+    ax2.set_ylabel('Cone angle [°]')
+    ax2.set_xlim([0,np.max(RelOffCents)])
+    
+    sc2 = ax2.scatter(meshOC,meshA/(2*np.pi)*360,c=SheetWide*360/(2*np.pi),cmap='cividis',s=pointSize)
+    
+    cbar2 = plt.colorbar(sc2)
+    cbar2.set_label('Sheet opening [°]')
+    fig2.tight_layout()
+    
+    # kinetic energy in the jet
+    fig3,ax3 = plt.subplots(dpi=150,figsize = (7,6)) 
+    ax3.set_title('DropSize/ConeSize : ' + str(RelDropDiam) + '°')
+    ax3.set_xlabel('Offcent/ConeRadius')
+    ax3.set_ylabel('Cone angle [°]')
+    ax3.set_xlim([0,np.max(RelOffCents)])
+    
+    sc3 = ax3.scatter(meshOC,meshA/(2*np.pi)*360,c=JetNRJ*1000,cmap='jet',s=pointSize)
     
     cbar3 = plt.colorbar(sc3)
     cbar3.set_label('Maximum kinetic energy in jet [mJ]')
